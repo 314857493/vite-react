157e7fba3ffc24ec9c44a895483847bb067ac4db
jiaxingdong <jiaxingdong@jd.com>
2022-01-11 11:45:51 +0800

    init

---
+++ src/Layout/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+// import Sider from "./Sider";
+import Content from "./Content";
+// import RouteBox from "../";
+import styles from "./index.module.less";
+
+function index({ children }) {
+  return (
+    <div className={styles.layout}>
+      <Header></Header>
+      <div className={styles.content}>
+        {/* <Sider></Sider> */}
+        <Content>{children}</Content>
+      </div>
+    </div>
+  );
+}
+
+export default index;
