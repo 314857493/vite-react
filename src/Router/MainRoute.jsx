3cb2e093b34ded3de6198cfacb850ef890e9977b
jiaxingdong <jiaxingdong@jd.com>
2022-01-14 13:18:46 +0800

    update handle路由为/main/*的404路由

--- src/Router/MainRoute.jsx
+++ src/Router/MainRoute.jsx
@@ -3,6 +3,7 @@ import { Route, Switch, useRouteMatch } from "react-router-dom";
 import routes from "./mainRoutes";
 import Layout from "@/Layout";
 import Sider from "@/page/Main/Sider";
+import NoMatch from "@/page/NoMatch";
 
 const MainRoute = () => {
   const match = useRouteMatch();
@@ -38,7 +39,16 @@ const MainRoute = () => {
     <Layout>
       <Sider />
       <div style={{ flex: 1 }}>
-        <Switch>{routes.map((item) => filteRoute(item))}</Switch>
+        <Switch>
+          {routes.map((item) => filteRoute(item))}
+          <Route
+            path="*"
+            render={(props) => {
+              document.title = "404";
+              return <NoMatch {...props} />;
+            }}
+          />
+        </Switch>
       </div>
     </Layout>
   );
